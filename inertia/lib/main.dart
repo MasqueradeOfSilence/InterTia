2fbdba33ff8f5d3a885b8bca337edc5e18d7e4e7
Alex <alexcn711@gmail.com>
2021-04-09 14:49:00 -0600

    Progress on texts and tabs

--- inertia/lib/main.dart
+++ inertia/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:inertia/strings.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.black,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
@@ -72,41 +73,49 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Image.asset("assets/images/logo1.png"),
-            DefaultTabController(length: 4, child: TabBar(
-                tabs: [
-                  Tab(text: "CAPTURE"),
-                  Tab(text: "AUTOPILOT"),
-                  Tab(text: "GOALS"),
-                  Tab(text: "MOSQUITO"),
-                ],
-              ),
+    return DefaultTabController(
+      length: 6,
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 200,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(""),
+          flexibleSpace: Image.asset("assets/images/logo1.png"),
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: [
+              Tab(text: "CAPTURE"),
+              Tab(text: "AUTOPILOT"),
+              Tab(text: "GOALS"),
+              Tab(text: "MOSQUITO"),
+              Tab(text: "ABOUT"),
+              Tab(text: "ACCOUNT"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Center(
+              child: Text("Capture", style: TextStyle(color: Colors.white60)),
+            ),
+            Center(
+              child: Text("Autopilot", style: TextStyle(color: Colors.white60)),
+            ),
+            Center(
+              child: Text("Goals", style: TextStyle(color: Colors.white60)),
+            ),
+            Center(
+              child: Text("Mosquito", style: TextStyle(color: Colors.white60)),
+            ),
+            Column(
+              children: [
+                Text(Strings.aboutUs, style: TextStyle(color: Colors.white)),
+                // Image.asset("assets/images/shatter12.gif"),
+              ],
+            ),
+            Center(
+              child: Text("Account", style: TextStyle(color: Colors.white60)),
             ),
           ],
         ),
---
+++ inertia/lib/strings.dart
@@ -0,0 +1,26 @@
+class Strings extends Object {
+  static String aboutUs = """
+  About This App
+  
+  InerTia is Intelligent Thinking. Designed and built by Evenstar Creative, our mission is to help you achieve your life's goals. Our belief is that this is accomplished by 4 key principles:
+  
+  - Setting and tracking goals.
+  - Setting aside regular time to work on these goals: an autopilot schedule.
+  - Focusing on deep work during this time.
+  - Getting boring, logistical "mosquito tasks" completed as quickly and efficiently as possible, so you have more time to focus on the big stuff.
+  
+  The principle of inertia states that an object in rest stays in rest, and an object in motion stays in motion, unless acted upon by an outside force. Our goal is to help you become your own force. To stay in motion.
+  
+  This approach is inherently discomfortable. Being discomfortable, in our view, is different than being uncomfortable, and far superior. Being uncomfortable implies experiencing damage and pain, like pushing yourself too hard during a workout and getting injured. Being discomfortable implies that you are stepping out of your comfort zone regularly, but in a healthy manner, with plenty of breaks and time to recuperate. In fact, getting less than 7 hours of sleep per night will actually harm your overall score in the app.
+  
+  The biggest challenge is to implement a system that keeps this as lightweight and simple as possible. InerTia is our valiant effort to achieve that. This system is based loosely on a few key principles taught by Cal Newport in his blog Study Hacks, in application format.
+  
+  At Evenstar Creative, we don't believe in dabbling. We believe in focusing on as many areas as desired, yes, but with an intent to master each one. We reject the modern-day zeitgeist of both specialization and generalization, and instead focus on true mastery. At Evenstar, we believe in being a master of many trades. At Evenstar, we believe in lifelong commitment, and being "in it to win it".
+  
+  Designed by Alex Neville, a software engineer/writer/runner/weight lifter/digital artist/theme park enthusiast/musician/PLC programmer/IT guy with big dreams and predominantly inattentive ADHD. I've always had trouble with the big question: How do I balance doing everything that I love? Because I refuse to drop a single thing. With InerTia, our hope is that you'll never have to.
+  
+  For questions, comments, or bugs, contact Alex Neville at softwaredev [at] alexanderneville [dot] com.
+  
+  Don't let your dreams get shattered. 
+  """;
+}
